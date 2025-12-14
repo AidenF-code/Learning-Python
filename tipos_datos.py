0e85f6c67d8989dea1f9a36db60d404e61f4293c
AidenF <andresfernandez1310@gmail.com>
2025-12-13 22:55:16 -0500

    Actualizacion

---
+++ tipos_datos.py
@@ -0,0 +1,34 @@
+# Programa: Ficha de héroe
+"""
+Las variables cuentan con diferentes tipos de datos como lo son los numeros enteros, cadenas de texto, valores booleanos
+entre otros, a continuacion se deja la representacion de los tipos de datos basicos:
+"""
+
+#str o Strings (Cadena de texto), para este tipo de dato es necesario que el valor esté entre comillas dobles o sencillas.
+nombre = 'Daniela'
+habilidad = "Leer la mente"
+
+# int (Números enteros)
+age = 25
+misiones_completadas =17
+
+# float (Números decimales)
+stamina = 65.7
+
+
+# bool (Booleanos)
+tiene_mas_poderes = False
+es_villana = True
+
+# NoneType (Sin valor)
+compania_actual = None
+
+print("*"*30)
+print("Nombre de la heroína:", nombre)
+print("Poder: ", habilidad)
+print("Edad: ", age)
+print("Misiones completadas con exito: ", misiones_completadas)
+print("Nivel de Stamina: ", stamina)
+print("¿Es una villana?" , es_villana)
+print("¿Tiene más poderes?" , tiene_mas_poderes)
+print("*"*30)
